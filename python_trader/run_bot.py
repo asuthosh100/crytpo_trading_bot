6421723199a68c6f09f73eec1c5a1d6658548ad8
asuthosh100 <asuthoshanandaram.com>
2024-11-13 11:57:04 -0600

    Creating Portfolio with features like adding multiple positions, deleting postions, and also checks if the symbol is profitable

--- python_trader/run_bot.py
+++ python_trader/run_bot.py
@@ -1,13 +1,27 @@
 from alpaca_trader import Alpacatrader 
+from portfolio import Portfolio
 
 def main(): 
 
-    client_id = 'PKVUX2M5K5YZWCK8E2JN'
-    secret_key = 'iK5aTBDK0VRjaVSTjeZHaqHMPbMCndkWNYrh9Hcl'
-    alpaca_trader = Alpacatrader(client_id=client_id, secret_key=secret_key)
+    # client_id = ''
+    # secret_key = ''
+    # alpaca_trader = Alpacatrader(client_id=client_id, secret_key=secret_key)
 
-    print("Alpaca Session Created: ", alpaca_trader.session)
+    # print("Alpaca Session Created: ", alpaca_trader.session)
 
+    portfolio = Portfolio()
+
+    positions = [
+    {"symbol": "GOOGL", "asset_type": "Equity", "quantity": 5, "purchase_price": 2800.0, "purchase_date": "2024-10-15"},
+    {"symbol": "TSLA", "asset_type": "Equity", "quantity": 3, "purchase_price": 700.0, "purchase_date": "2024-11-01"}
+]
+    
+    portfolio.add_positions(positions)
+    print(portfolio.positions)
+
+
+    portfolio.remove_postion('TSLA')
+    print(portfolio.positions)
 
 
 if __name__ == "__main__":
